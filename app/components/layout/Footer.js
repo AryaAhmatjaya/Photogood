7c09cbd689dbc2a7c6675d7e97bee00eb66e93e7
laksamanaap <laksamana.arya1412@gmail.com>
2024-02-24 03:02:32 +0700

    feat : fix ui and fetch comment

--- app/components/layout/Footer.js
+++ app/components/layout/Footer.js
@@ -107,7 +107,7 @@ export const Footer = () => {
         {/* <!-- Copyright --> */}
         <div
           class="text-center p-3"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
           © 2024 Copyright:
           <a class="text-black" href="https://mdbootstrap.com/">
